f7ab4d69248fd33ebf59ad33038bbcae89ba87b4
Thibaud Fabre <thibaud@aztech.io>
2016-10-03 10:21:24 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# queue-component
+
+[![License][badge-license]][license]
+[![Packagist][badge-packagist]][packagist]
+[![Travis][badge-travis]][travis]
+[![Coverage][badge-coverage]][coverage]
+[![Scrutinizer][badge-quality]][quality]
+[![Packagist][badge-downloads]][downloads]
+
+alchemy/worker-component is a library providing a simple worker and dispatcher for use with messages queues
+
+## Installation
+
+The only supported installation method is via [Composer](https://getcomposer.org). Run the following command to require 
+the package in your project:
+
+```
+composer require alchemy/worker-component
+```
+
+## Quickstart guide
+
+[badge-license]: https://img.shields.io/packagist/l/alchemy/worker-component.svg?style=flat-square
+[badge-packagist]: https://img.shields.io/packagist/v/alchemy/worker-component.svg?style=flat-square
+[badge-travis]: https://img.shields.io/travis/alchemy-fr/worker-component.svg?style=flat-square
+[badge-coverage]: https://img.shields.io/scrutinizer/coverage/g/alchemy-fr/worker-component.svg?style=flat-square
+[badge-quality]: https://img.shields.io/scrutinizer/g/alchemy-fr/worker-component.svg?style=flat-square
+[badge-downloads]: https://img.shields.io/packagist/dt/alchemy/worker-component.svg?style=flat-square
+
+[license]: https://github.com/alchemy-fr/worker-component/LICENSE
+[packagist]: https://packagist.org/packages/alchemy/worker-component
+[travis]: https://travis-ci.org/alchemy-fr/worker-component
+[coverage]: https://scrutinizer-ci.com/g/alchemy-fr/worker-component/?branch=master
+[quality]: https://scrutinizer-ci.com/g/alchemy-fr/worker-component/
+[downloads]: https://packagist.org/packages/alchemy/worker-component/stats
---
+++ src/WorkerInvoker.php
@@ -0,0 +1,112 @@
+<?php
+
+/*
+ * This file is part of parade-universal.
+ *
+ * (c) Alchemy <info@alchemy.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Alchemy\Worker;
+
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
+use Symfony\Component\Process\Exception\RuntimeException as ProcessRuntimeException;
+use Symfony\Component\Process\ProcessBuilder;
+
+class WorkerInvoker implements LoggerAwareInterface
+{
+    /**
+     * @var string
+     */
+    private $environment;
+
+    /**
+     * @var string
+     */
+    private $command = 'workers:run-worker';
+
+    /**
+     * @var string
+     */
+    private $binaryPath;
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * @var ProcessPool
+     */
+    private $processPool;
+
+    public function __construct(ProcessPool $processPool, $environment)
+    {
+        $this->binaryPath = $_SERVER['SCRIPT_NAME'];
+        $this->environment = $environment;
+        $this->processPool = $processPool;
+        $this->logger = new NullLogger();
+    }
+
+    /**
+     * Sets a logger instance on the object
+     *
+     * @param LoggerInterface $logger
+     * @return null
+     */
+    public function setLogger(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    /**
+     * @param string $messageType
+     * @param string $payload
+     */
+    public function invokeWorker($messageType, $payload)
+    {
+        $process = $this->processPool->getWorkerProcess([
+            $this->binaryPath,
+            $this->command,
+            sprintf('-e=%s', $this->environment),
+            '-vv',
+            $messageType,
+            $this->createPayloadFile($payload)
+        ], getcwd());
+
+        $this->logger->debug('Invoking shell command: ' . $process->getCommandLine());
+
+        try {
+            $process->start([$this, 'logWorkerOutput']);
+        } catch (ProcessRuntimeException $e) {
+            $process->stop();
+
+            throw new \RuntimeException(sprintf('Command "%s" failed: %s', $process->getCommandLine(),
+                $e->getMessage()), 0, $e);
+        }
+    }
+
+    private function createPayloadFile($payload)
+    {
+        $path = tempnam(sys_get_temp_dir(), 'parade_wk_');
+
+        if (file_put_contents($path, $payload) === false) {
+            throw new \RuntimeException('Cannot write payload file to path: ' . $path);
+        }
+
+        return $path;
+    }
+
+    public function logWorkerOutput($stream, $output)
+    {
+        if ($stream == 'err') {
+            $this->logger->error($output);
+        } else {
+            $this->logger->info($output);
+        }
+    }
+}
